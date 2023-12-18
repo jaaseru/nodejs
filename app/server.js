c3eb06260674aebf62868474fa4dc67916916c7b
jaaseru <julian.aaserud@onio.com>
2023-12-18 11:17:22 +0100

    update

--- app/server.js
+++ app/server.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // }));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'app/views'));
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.get('/', (req, res) => {
