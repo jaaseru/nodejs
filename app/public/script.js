3c088ebaf6f2c4edf751c662b0b16ac44ecda718
jaaseru <julian.aaserud@onio.com>
2023-12-20 08:27:29 +0100

    update

--- app/public/script.js
+++ app/public/script.js
@@ -378,7 +378,7 @@ function updateMetricPlot(elementId, data, title) {
 
         let layout = {
             title: title,
-            margin: { t: 20, l: 5, r: 30, b: 15 },
+            margin: { t: 15, l: 0, r: 32, b: 15 },
             autosize: true,
             titlefont: { size: 12 },
             xaxis: { 
